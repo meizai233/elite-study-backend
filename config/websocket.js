c8a9cdd1c12b4ffe3e9f7973771834e0a9bef543
fengwanyan <fengwy1@chinatelecom.cn>
2024-09-21 14:39:56 +0800

    redis发布订阅模式 实现不同节点的实时消息更新

--- config/websocket.js
+++ config/websocket.js
@@ -1,4 +1,11 @@
 const { Server } = require("socket.io");
+const Redis = require("ioredis");
+// redis发布的api
+const clientPublish = new Redis({ port: 6379, host: "47.121.207.171", password: "Qweasd123" });
+// redis订阅api
+const clientSubscribe = new Redis({ port: 6379, host: "47.121.207.171", password: "Qweasd123" });
+
+clientSubscribe.subscribe("chat");
 
 const websocket = (server) => {
   // 实例化socket
@@ -8,8 +15,14 @@ const websocket = (server) => {
     console.log("有客户端链接进来了");
     // 监听bulletChat事件
     socket.on("bulletChat", (info) => {
-      io.emit("message", info);
+      // 每次发弹幕时 发布chat事件给所有subscriber
+      clientPublish.publish("chat", info);
     });
+  });
+  // 订阅者收到消息后 执行websocket的消息推送给客户端
+  // 新消息被发布到channel时 message事件被触发
+  clientSubscribe.on("message", (channel, message) => {
+    io.emit("message", message);
   });
 };
 module.exports = websocket;
