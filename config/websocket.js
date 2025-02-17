663a4a30a68e388e63784c5c0b70dabde98b68a4
fengwanyan <fengwy1@chinatelecom.cn>
2025-02-17 10:44:53 +0800

    ws证书配置测试

--- config/websocket.js
+++ config/websocket.js
@@ -1,5 +1,7 @@
 const { Server } = require("socket.io");
 const Redis = require("ioredis");
+const fs = require("fs");
+const https = require("https");
 // redis发布的api
 const clientPublish = new Redis({ port: 6379, host: "47.121.207.171", password: "Qweasd123" });
 // redis订阅api
@@ -8,11 +10,24 @@ const clientSubscribe = new Redis({ port: 6379, host: "47.121.207.171", password
 clientSubscribe.subscribe("chat");
 
 const websocket = (server) => {
-  // 实例化socket
-  const io = new Server(server, { cors: { origin: "*" } });
+  // SSL 配置
+  const options = {
+    key: fs.readFileSync("/ssl/cert.key"),
+    cert: fs.readFileSync("/ssl/cert.pem"),
+  };
+
+  // 实例化 socket
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
   // websocket 建立连接
   io.on("connection", (socket) => {
     // console.log("有客户端链接进来了");
+
     // 监听bulletChat事件
     socket.on("bulletChat", (info) => {
       // 每次发弹幕时 发布chat事件给所有subscriber
@@ -20,10 +35,12 @@ const websocket = (server) => {
       io.emit("message", info);
     });
   });
+
   // 订阅者收到消息后 执行websocket的消息推送给客户端
   // 新消息被发布到channel时 message事件被触发
   clientSubscribe.on("message", (channel, message) => {
     io.emit("message", JSON.parse(message));
   });
 };
+
 module.exports = websocket;
